a62782ce4dee603b19dd3154c11afbf374fdde3f
V.Chekholin <Chekholin@outlook.com>
2017-05-15 20:46:28 +0300

    Some appearance fixes

--- app/models/users.js
+++ app/models/users.js
@@ -1,4 +1,7 @@
-var mongoose = require('mongoose');
+var mongoose = require('mongoose'),
+	crypto = require('crypto'),
+	jwt = require('jsonwebtoken');
+  // Schema = mongoose.Schema;
 
 var userSchema = new mongoose.Schema({
 	email: {
@@ -13,3 +16,27 @@ var userSchema = new mongoose.Schema({
 	hash: String,
 	salt: String
 });
+
+userSchema.methods.setPassword = function(password){
+	this.salt = crypto.randomBytes(16).toString('hex');
+	this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+};
+
+userSchema.methods.validPassword = function(password) {
+	var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+	return this.hash === hash;
+};
+
+userSchema.methods.generateJwt = function() {
+	var expiry = new Date();
+	expiry.setDate(expiry.getDate() + 60);
+
+	return jwt.sign({
+		_id: this._id,
+		email: this.email,
+		name: this.name,
+		exp: parseInt(expiry.getTime() / 1000)
+	}, 'ThisIsSecret'/*process.env.JWT_SECRET*/); // <-- задать переменную
+};
+
+mongoose.model('User', userSchema);
