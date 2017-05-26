23e554e7084b8cecb3271b0fabae229e63dd9145
V.Chekholin <Chekholin@outlook.com>
2017-05-26 20:32:44 +0300

    Add Passport.js auth

---
+++ config/passport.js
@@ -0,0 +1,33 @@
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var mongoose = require('mongoose');
+var User = mongoose.model('User');
+
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+   User.getUserByUsername(username, function(err, user){
+    if(err) throw err;
+    if(!user){
+        return done(null, false, {message: 'Unknown User'});
+    }
+    User.comparePassword(password, user.password, function(err, isMatch){
+        if(err) throw err;
+        if(isMatch){
+            return done(null, user);
+        } else {
+            return done(null, false, {message: 'Invalid password'});
+        }
+    });
+   });
+  }));
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.getUserById(id, function(err, user) {
+    done(err, user);
+  });
+});
